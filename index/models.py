f8d77b5baeaad3f37c91921fbac3cddaa6859ca0
hjz15k6 <hjz15k6@gmail.com>
2015-10-22 21:23:36 +0800

    build models

--- index/models.py
+++ index/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class article(models.Model):
+	title = models.CharField(max_length = 140)
+	date = models.DateTimeField(auto_now_add = True)
+	content = models.TextField(blank = True, null = True)
