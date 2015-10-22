9db284c141a16d06e0de0d0c1581a53eb50238b6
hjz15k6 <hjz15k6@gmail.com>
2015-10-22 21:55:44 +0800

    display models

--- index/views.py
+++ index/views.py
@@ -2,6 +2,7 @@
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import article
 def index(request):
     return HttpResponse(u'''hello world<br>
     	<a href = 'list'>list</a><br>
@@ -12,13 +13,15 @@ def list(request):
 	alist = ['one', 'two', 'three']
 	return render(request, 'list.htm', {'testlist': alist})
 def main(request):
-	class article(object):
+	class wz(object):
 		def __init__(self, title='title', date='1970/1/1', contents='balablashenmade'):
 			self.title = title
 			self.date = date
 			self.contents = contents
-	a1 = article('wo shi yi')
-	a2 = article()
-	a3 = article(date='2012/12/21')
+	a1 = wz('wo shi yi')
+	a2 = wz()
+	a3 = wz(date='2012/12/21')
 	articlelist = [a1, a2, a3]
+	#article.objects.create(title = '111', content = 'biubiubiu')
+	articlelist = article.objects.all()
 	return render(request, 'main.htm',{'articlelist': articlelist})
